fd65e24d04fbc2c3a33d42751c53c669f5e4d55a
Julia <>
2021-11-23 19:00:02 +0100

    finalized styled components

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,19 +1,38 @@
+import React from "react";
 import styled from "styled-components";
+import { StyledToggle, StyledTogglePointer, StyledInput } from "./SharedStyles";
+import { useState } from "react";
 
-export const Toggle = ({ id, disaster }) => (
-    <div className="inlineWrapper">
-        <div className={`inline toggle${id}`} id={`filter${id}`}>
-            <input id={`layer${id}On`} type="radio" value={`layer${id}On`} name={`toggle${id}`} checked='checked' />
-            <input id={`layer${id}Off`} type="radio" value={`layer${id}Off`} name={`toggle${id}`} />
-            <div className= {`toggle__pointer color${id}`}></div>
-        </div>
-        <div className="inline">
+const colorObj = { "1":"#a8ddb5","2":"#2b8cbe","3":"#c51b8a","4":"#d8b365","5":"#8073ac" };
+
+export const Toggle = ({ id, disaster }) => {
+
+    const [checked, setChecked] = useState(true);
+    const toggleToggle = () => {
+        console.log(checked);
+        setChecked((prevState)=>!prevState);
+    };
+
+    console.log(checked);
+    return (
+        <div>
+            <StyledToggle color={colorObj[id]} id={`filter${id}`} onClick={toggleToggle}>
+                <StyledInput id={`layer${id}On`} type="radio" value={`layer${id}On`} name={`toggle${id}`} checked={checked} />
+                <StyledInput id={`layer${id}Off`} type="radio" value={`layer${id}Off`} name={`toggle${id}`} />
+                <StyledTogglePointer color={colorObj[id]} checked={checked}></StyledTogglePointer>
+            </StyledToggle>
             <ToggleTextStyled className = "disasterName">{disaster}</ToggleTextStyled>
+
         </div>
-    </div>
-);
+    );
+};
 
 
 const ToggleTextStyled = styled.p`
-    margin-left: 10px;
+    display: inline-block;
+    vertical-align: middle;
+    margin-right: 10px;
+    margin-left: 0px;
+    margin-top:5px;
+    margin-bottom:5px;
 `;
