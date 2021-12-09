228fb39584603a0779338ee8dd07b29287eccfbe
Julia <>
2021-12-09 18:17:42 +0100

    added tooltip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import MapGL, { Source, Layer } from "react-map-gl";
 import styled from "styled-components";
-import "./App.css";
 import { Legend } from "./Legend";
 import { Slider } from "./Slider";
 import { ToggleContainer } from "./ToggleContainer";
@@ -19,6 +18,27 @@ function App() {
         zoom: 2
     });
 
+    const [hoverInfo, setHoverInfo] = useState(null);
+
+    const onHover = useCallback(event => {
+        const {
+            features,
+            srcEvent: { offsetX, offsetY }
+        } = event;
+        const hoveredFeature = features && features[0];
+
+
+        setHoverInfo(
+            hoveredFeature
+                ? {
+                    feature: hoveredFeature,
+                    x: offsetX,
+                    y: offsetY
+                }
+                : null
+        );
+
+    }, []);
 
     return (
         <>
@@ -31,10 +51,18 @@ function App() {
                 mapStyle="mapbox://styles/jhjanicki/ckrukpn7t13zy18o82tq8o7wp"
                 interactiveLayerIds={["countriesFill"]}
                 mapboxApiAccessToken={MAPBOX_TOKEN}
+                onHover={onHover}
             >
                 <Source type="geojson" data={countries}>
                     <Layer {...dataLayer} beforeId='settlement-subdivision-label'/>
                 </Source>
+                {hoverInfo && (
+                    <TooltipStyled className="tooltip" style={{ left: hoverInfo.x, top: hoverInfo.y }}>
+                        <div>Country: {hoverInfo.feature.properties.ROMNAM}</div>
+                        <div>ND Gain Index: {hoverInfo.feature.properties.NDgain}</div>
+                    </TooltipStyled>
+                )}
+
             </MapGL>
             <SidepanelStyled>
                 <p> <b>Climate change effects by country</b></p>
@@ -61,5 +89,18 @@ const SidepanelStyled = styled.div`
   transition: 0.3s ease-in-out;
   box-shadow: 0 1px 1px 0 rgba(66, 66, 66, 0.08), 0 1px 3px 1px rgba(66, 66, 66, 0.16);
 `;
+
+const TooltipStyled = styled.div`
+    position: absolute;
+    margin: 8px;
+    padding: 6px;
+    background: white;
+    color: black;
+    max-width: 300px;
+    font-size: 14px;
+    z-index: 9;
+    pointer-events: none;
+    border-radius: 5px;
+  `;
 
 export default App;
